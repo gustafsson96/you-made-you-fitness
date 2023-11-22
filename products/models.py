fe28bbb3f8f3ba930c6d04eaf09ae6e5a6ee0c81
gustafsson96 <>
2023-11-22 16:25:38 +0000

    Add model for workout guides

--- products/models.py
+++ products/models.py
@@ -7,4 +7,14 @@ TYPE_OF_GUIDE = (
 )
 
 
-class Guides(models.Model):
+class Guide(models.Model):
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    type_of_guide = models.CharField(max_length=50, choices=TYPE_OF_GUIDE)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
