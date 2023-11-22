d16d5ddeda5dda39203374022dd583d4a8a93756
gustafsson96 <>
2023-11-22 18:07:19 +0000

    Add views and template for guides in products app

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Guide
 
-# Create your views here.
+
+def products_guides(request):
+    """ A view to show all workout guides """
+
+    guides = Guide.objects.all()
+
+    context = {
+        'guides': guides,
+    }
+
+    return render(request, "products/products.html", context)
