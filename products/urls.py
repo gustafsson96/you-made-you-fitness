95d5f589d88143b076fd8abb00d493e18d89ea56
gustafsson96 <>
2023-11-28 16:28:53 +0000

    Set up a detail page for the individual guides

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.products_guides, name='products_guides')
+    path('', views.products_guides, name='products_guides'),
+    path('<guide_id>/', views.product_guide_detail, name='product_guide_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Guide
 
 
@@ -12,3 +12,15 @@ def products_guides(request):
     }
 
     return render(request, "products/products.html", context)
+
+
+def product_guide_detail(request, guide_id):
+    """ A view to show each guide's details"""
+
+    guide = get_object_or_404(Guide, pk=guide_id)
+
+    context = {
+        'guide': guide,
+    }
+
+    return render(request, "products/product_guide_detail.html", context)
