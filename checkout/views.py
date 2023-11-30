7daf8de604a8680b7f1c072e88da82620a6c4686
gustafsson96 <>
2023-11-30 16:53:55 +0000

    Set up stripe and add styling

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51O4QqsJ3bX1eIb24JUAlNmcSX4Z6gEvWBGD96PVPQi00LFuyqFxtvEafIhjnK1c9tqCO8QPwClKFWwffn29efvBd00P5swrlGv',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
