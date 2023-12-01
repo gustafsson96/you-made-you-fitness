eb9a2c726391d8c106e2e11bfc83d67e90dc0751
gustafsson96 <>
2023-12-01 20:03:11 +0000

    Create views and templates for community pages

--- community/views.py
+++ community/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Recipe, Workout, OtherPost
 
-# Create your views here.
+
+class RecipeList(generic.ListView):
+    model = Recipe
+    queryset = Recipe.objects.filter(status=1).order_by('-created_on')
+    template_name = 'recipe_post_page.html'
+    paginate_by = 5
+
+
+class WorkoutList(generic.ListView):
+    model = Workout
+    queryset = Workout.objects.filter(status=1).order_by('-created_on')
+    template_name = 'workout_post_page.html'
+    paginate_by = 5
+
+
+class OtherPostList(generic.ListView):
+    model = OtherPost
+    queryset = OtherPost.objects.filter(status=1).order_by('-created_on')
+    template_name = 'other_post_page.html'
+    paginate_by = 5
