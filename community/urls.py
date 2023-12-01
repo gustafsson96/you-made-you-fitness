a06fd1793f3ae6b951b6835ae6ebe56b68b9a835
gustafsson96 <>
2023-12-01 21:08:44 +0000

    Render content from database to community post pages

--- community/urls.py
+++ community/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('community/', views.community, name="community"),
-    path('recipe_posts/', views.recipe_list, name='recipe'),
-    path('workout_posts/', views.workout_list, name='workout'),
-    path('other_posts/', views.other_post_list, name='other'),
+    path('recipe_posts/', views.recipe_list, name='recipes'),
+    path('workout_posts/', views.workout_list, name='workouts'),
+    path('other_posts/', views.other_post_list, name='other_posts'),
 ]
