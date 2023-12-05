91fbac76033c3832f5724601850796f643024fc0
gustafsson96 <>
2023-12-05 08:29:33 +0000

    Change url path name for community start page

--- community/urls.py
+++ community/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('community/', views.community, name="community"),
+    path('start', views.community, name="community"),
     path('recipe_posts/', views.recipe_list, name='recipes'),
     path('workout_posts/', views.workout_list, name='workouts'),
     path('other_posts/', views.other_post_list, name='other_posts'),
