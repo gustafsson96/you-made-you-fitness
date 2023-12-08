1106b74f5270aefa35fe4c37b9e35ed54589e5a3
gustafsson96 <>
2023-12-08 11:24:27 +0000

    Add additional code to link sign up info to profile

--- userprofile/signals.py
+++ userprofile/signals.py
@@ -1,9 +1,10 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from allauth.account.signals import user_signed_up
+from django.contrib.auth.models import User
 from .models import UserProfile
 
 
-@receiver(user_signed_up)
-def create_user_profile(sender, request, user, **kwargs):
-    UserProfile.objects.create(user=user)
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
 
 
 def user_profile(request):
     """ view for user profile """
 
-    profile = UserProfile.objects.all()
+    profile = get_object_or_404(UserProfile, user=request.user)
 
     context = {
         'profile': profile,
