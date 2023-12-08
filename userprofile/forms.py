92570d82829442c89dbf360281bf6b60d9c50949
gustafsson96 <>
2023-12-08 11:41:34 +0000

    Add form for user profiles

---
+++ userprofile/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import UserProfile
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user'),
+
+        fields = ['bio', 'fun_fact', 'profile_picture']
+        placeholders = {
+            'bio': 'Bio',
+            'fun_fact': 'Fun Fact',
+            'profile_picture': 'Profile Picture',
+        }
+
+form = ProfileForm()
