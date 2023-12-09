32e4281904570a6bcbc37fa08164314fb5b48d3d
gustafsson96 <>
2023-12-09 20:19:10 +0000

    Add functionality to delete items from shopping cart

--- cart/views.py
+++ cart/views.py
@@ -25,19 +25,12 @@ def add_to_cart(request, item_id):
 
 
 def remove_from_cart(request, item_id):
-    """Remove the item from the shopping bag"""
+    """remove an item from the shopping cart"""
 
     try:
-        size = None
-        if 'product_size' in request.POST:
-            size = request.POST['product_size']
         cart = request.session.get('cart', {})
 
-        if size:
-            del cart[item_id]['items_by_size'][size]
-            if not cart[item_id]['items_by_size']:
-                cart.pop(item_id)
-        else:
+        if item_id in cart:
             cart.pop(item_id)
 
         request.session['cart'] = cart
